019a2f590e5abe26c42693f9dc73e20831128136
Bennet Gallein <bennet@intranetproject.net>
2017-10-10 01:32:09 +0200

    Trying to get the session function to run..

--- src/Paladins.php
+++ src/Paladins.php
@@ -3,7 +3,8 @@ namespace Paladins;
 
 class Paladins {
 
-
+    private $timestamp;
+    private $session_id;
 
     private $devId;
     private $authKey;
@@ -26,9 +27,32 @@ class Paladins {
         return $this->req("ping" . $this->format);
     }
     public function connect() {
-        return $this->req("createsession" . $this->format . "/" . $this->devId . "/" . $this->getSignature("createsession") . "/" . $this->getTimestamp());
+        $data =  $this->req("createsession" . $this->format . "/" . $this->devId . "/" . $this->getSignature("createsession") . "/" . $this->getTimestamp());
+        $session = json_decode($data, true);
+        print_r($session);
+
+        // WIP
+        
+        /*
+        if ($session['ret_msg'] === "Approved") {
+            $this->session_id = $session['session_id'];
+            $this->timestamp = $session['timestamp'];
+        } else {
+            return "Failed to get a session ID.";
+        } */
     }
     public function req($url) {
+
+        // WIP
+
+        // if time_now >= timestamp + 15 min
+        /*echo "API:" . strtotime($this->timestamp) . "<br>";
+        echo "Current:" . strtotime("-15 minutes") . "<br>";
+        echo strtotime($this->timestamp) < strtotime("-15 minutes") . "<br>";
+        echo strtotime($this->timestamp) > strtotime("-15 minutes") . "<br>";
+        /*if (strtotime($this->timestamp) < strtotime("-15 minutes")) {
+            $this->connect();
+        }*/
         $ch = curl_init();
         curl_setopt_array($ch, array(
             CURLOPT_URL => $this->baseURL . "/" . $url,
@@ -39,11 +63,14 @@ class Paladins {
         ));
         $data = curl_exec($ch);
         curl_close($ch);
-        $data = json_decode($data, true);
+        //$data = json_decode($data, true);
         return $data;
     }
     public function getSignature($reqType) {
         return md5($this->devId . $reqType . $this->authKey . $this->getTimestamp());
+    }
+    public function getChampions() {
+        return $this->req("getchampions" . $this->format . "/" . $this->devId . "/" . $this->getSignature("getchampions") . "/" . $this->session_id . "/" . $this->getTimestamp() . "/" . $this->lang);
     }
     public function getTimestamp() {
         //$date = new \DateTime();
