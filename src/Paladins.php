e91d997bf301e7443a1f338ce8550f457c3b6fc9
Bennet Gallein <bennet@intranetproject.net>
2017-10-10 00:58:22 +0200

    Added ping() and connect() event, lovely structure

--- src/Paladins.php
+++ src/Paladins.php
@@ -3,6 +3,8 @@ namespace Paladins;
 
 class Paladins {
 
+
+
     private $devId;
     private $authKey;
     private $format;
@@ -11,11 +13,40 @@ class Paladins {
     private $baseURL = 'http://api.paladins.com/paladinsapi.svc';
 
     public function __construct($devId, $authKey, $format = "json", $lang = 1) {
+
+        date_default_timezone_set('UTC');
+
         $this->devId = $devId;
         $this->authKey = $authKey;
         $this->format = $format;
         $this->lang = $lang;
-
-
+    }
+    /* API METHODS */
+    public function ping() {
+        return $this->req("ping" . $this->format);
+    }
+    public function connect() {
+        return $this->req("createsession" . $this->format . "/" . $this->devId . "/" . $this->getSignature("createsession") . "/" . $this->getTimestamp());
+    }
+    public function req($url) {
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $this->baseURL . "/" . $url,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_FOLLOWLOCATION => 1,
+            CURLOPT_VERBOSE        => 1,
+            CURLOPT_SSL_VERIFYPEER => 0
+        ));
+        $data = curl_exec($ch);
+        curl_close($ch);
+        $data = json_decode($data, true);
+        return $data;
+    }
+    public function getSignature($reqType) {
+        return md5($this->devId . $reqType . $this->authKey . $this->getTimestamp());
+    }
+    public function getTimestamp() {
+        //$date = new \DateTime();
+        return gmdate('YmdHis');
     }
 }
