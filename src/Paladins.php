63c72866b4825184a64ba361b0c749ef28480842
Bennet Gallein <bennet@intranetproject.net>
2017-10-09 19:25:39 +0200

    Added basic file structure

--- src/Paladins.php
+++ src/Paladins.php
@@ -8,10 +8,14 @@ class Paladins {
     private $format;
     private $lang;
 
-    public function __construct($devId, $authKey, $format, $lang) {
+    private $baseURL = 'http://api.paladins.com/paladinsapi.svc';
+
+    public function __construct($devId, $authKey, $format = "json", $lang = 1) {
         $this->devId = $devId;
         $this->authKey = $authKey;
         $this->format = $format;
         $this->lang = $lang;
+
+
     }
 }
---
+++ src/const/Const.php
@@ -0,0 +1,20 @@
+<?php
+namespace Paladins;
+
+class Const {
+
+    // languages
+    const ENGLISH = 1;
+    const GERMAN = 2;
+    const FRENCH = 3;
+    const SPANISH = 7;
+    const SPANISHLA = 9;
+    const PORTUGUESE = 10;
+    const RUSSIAN = 11;
+    const POLISH = 12;
+    const TURKISH = 13;
+
+    // Response Formats
+    const JSON = 'Json';
+    const XML = 'xml';
+}
